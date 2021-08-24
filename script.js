8590859e55148a67edc8e7b75152fe2b34c461aa
jleynes <38470756+jleynes@users.noreply.github.com>
2021-08-24 16:48:11 -0700

    Updated file code

--- script.js
+++ script.js
@@ -1,3 +1,61 @@
+// Variables
+let displayValue = "0";
+
+let numberA = "";
+let numberB = "";
+let numberTotal = "";
+
+let operator = "";
+
+// DOM lines
+// DOM to change displayOutput
+let displayOutput = document.getElementById('displayOutput');
+displayOutput.innerHTML = displayValue.toString();
+
+// DOM to change calculationOutput
+let calculationOutput = document.getElementById('calculationOutput');
+calculationOutput.innerHTML = numberA.toString() + operator.toString();
+
+
+const numbers = document.querySelectorAll('.number');
+const decimal = document.querySelector('.decimal');
+const operators = document.querySelectorAll('.operator');
+const clear = document.querySelector('.clear').addEventListener('click', clearAll);
+
+
+// Adds an event listener to each button to add to the number variables
+numbers.forEach(number => {
+    number.addEventListener('click', e => {
+      if (operator == "") {
+        numberA += e.target.innerText;
+        calculationOutput.innerHTML = numberA.toString()
+        console.log(numberA);
+      } else {
+        numberB += e.target.innerText;
+        console.log(numberB);
+      }
+    });
+});
+
+// Adds an event listener to the operator buttons and calculates based on that
+operators.forEach(operatorChoice => {
+  operatorChoice.addEventListener('click', e => {
+    if (e.target.id !== "equals") {
+      operator = e.target.id;
+
+      console.log(numberA);
+      console.log(operator);
+
+    } else {
+      numberTotal = operate(operator, parseInt(numberA), parseInt(numberB));
+
+      console.log(numberB);
+      console.log(numberA);
+      console.log(operator);
+    }
+  });
+});
+
 // calculation functions
 function add(num1, num2) {
   return num1 + num2;
@@ -37,4 +95,15 @@ function operate(choice, num1, num2) {
     default:
       break;
   }
+}
+
+// function that resets calculator
+function clearAll() {
+  this.displayValue = "0";
+
+  this.numberA = "";
+  this.numberB = "";
+  numberTotal = "";
+
+  this.operator = undefined;
 }
