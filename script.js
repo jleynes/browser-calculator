c935ca54c698307b7df9bc6f272375988c1c48c8
jleynes <jleynes@asu.edu>
2021-08-23 23:54:47 -0700

    Updated code

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-// calculations
+// calculation functions
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  if(num2 === 0) {
+    return "Bruh no."
+  } else {
+    return num1 / num2;
+  }
+}
+
+// function that calls calculation function depending on operator choice
+function operate(choice, num1, num2) {
+  switch (choice) {
+    case "add":
+      return add(num1, num2);
+      break;
+    case "subtract":
+      return subtract(num1, num2);
+      break;
+    case "multiply":
+      return multiply(num1, num2);
+      break;
+    case "divide":
+      return divide(num1, num2);
+      break;
+    default:
+      break;
+  }
+}
